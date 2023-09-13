ed6a42d10438622d4bac3d8f3b710476d1b04d39
Douglas Gordon <gordononline@bigpond.com>
2023-09-13 18:21:08 +1000

    Learnings on dictionaries

--- learnings.py
+++ learnings.py
@@ -9,6 +9,11 @@ print(f"{MENU[get_input()]['ingredients']['water']} ml")
 
 from menu import MENU, resources
 
-print(MENU["latte"]["ingredients"])
-print(MENU["latte"])
-print(MENU)
+print(MENU.keys()) # just the keys
+print(MENU.items()) # items as a list
+print(MENU.values()) # values without keys
+print(MENU["latte"]["ingredients"]["milk"]) # lowest value
+print(MENU["latte"]["ingredients"]) #second lowest value
+print(MENU["latte"]["cost"]) # third lowest or second highest value
+print(MENU["latte"]) # highest value
+print(MENU) # all items as dictionary
